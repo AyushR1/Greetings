2fac57023b3058587d02d876a71c2a3b019941cd
AyushR1 <ayushrathore12501@gmail.com>
2022-10-15 23:38:20 +0530

    Add simple toast
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- app/src/main/java/com/ayushr1/greetings/MainActivity.kt
+++ app/src/main/java/com/ayushr1/greetings/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.ayushr1.greetings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun ButtonClick(view: View) {
+        Toast.makeText(this,"Button was cliucked",Toast.LENGTH_LONG).show()
     }
 }
